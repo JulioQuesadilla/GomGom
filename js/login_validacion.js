1390670941bc4c4ea328fedc475fa934c10322cc
JA Gonzalez <jatgonzalez33@gmail.com>
2022-07-28 19:01:49 -0500

    Docs/Refactor: Se añaden comentarios al código y se refactoriza. Vincula JSON

--- js/login_validacion.js
+++ js/login_validacion.js
@@ -1,57 +1,103 @@
-let loginInfo = {
-    "mail": "abueloco@gmail.com",
-    "password": "abuehackerman123"
-}
+/**
+ * SCRIPT PARA VALIDACIÓN DE LOGIN
+ * jatg-eca el abuehackerman pal mundo clarokezi ultrayes sí señor_/A/e
+ * 
+ * Realizado para GomGom
+ * Product Owner: Hector Mora
+ * Scrums: MoisesCH14 y el abuelin
+ * Lead Devs: Venus, JulionQuesadillonALCESARLOKEESDELCESAR, LeoKawicha, AdriánLimón
+ * (textrañamos,Lau)
+ * 
+ * JAVAJAVA-DO!
+ *  
+ * 
+*/
 
+/*Vilma: al iniciar el código se hace un fetch de un JSON
+ que simula una base de datos de usuarios registrados y una variable
+ que guarda la información de login */
+let loginInfo;
+fetchInfo("../json/login.json");
+
+/*Este es un (piedra) formulario para tabletas y celulares (prehistóricos)
+ Se almacena el input para correo y la contraseña del piedraFormulario,
+ y se recuerda el botón de login que acompaña en la navbar que tiene offcanvas*/
 let formLoginNav = document.forms["loginDos"];
 let correoLoginNav = formLoginNav["correoDos"];
 let contraseniaLoginNav = formLoginNav["contraDos"];
 let botonLoginNav = document.getElementById("botonLoginDos");
 
+/*Estas variables declaradas recuerdan un (piedra) formulario para verlo
+ en la piedracomputadora. Igual que el formulario para tabletas y celulares,
+ se almacenan los inputs para correo, constraseña, y se recuerda el botón de login
+ de la navbar para piedraComputadoras*/
 let formLoginPc = document.forms["loginUno"];
 let correoLoginPc = formLoginPc["correoUno"];
 let contraseniaLoginPc = formLoginPc["contraUno"];
 let botonLoginPc = document.getElementById("botonLoginUno");
 
 
+/* (JAVAJAVADO!) Se les pone un dino Escuchador de Eventos a los botones de
+ inicio de sesión, para que al momento de escuchar un click de la persona cliente,
+ se valide el inicio de sesión para PC o celular*/
 botonLoginNav.addEventListener("click", validateNavLogin);
 botonLoginPc.addEventListener("click", validatePcLogin);
-console.log(typeof(loginInfo.mail));
 
+/*En el HTML se puso un pequeño párrafo que indica si hay algún error
+ al iniciar sesión.
+ El declararlos variables aquí es una herramienta secreta que nos ayudará más tarde*/
 let checkLoginUno = document.getElementById("validaLogin");
 let checkLoginDos = document.getElementById("validaLoginNav");
         
 
+/**
+ *Una función asíncrona que se realiza para celulares, al dar click en iniciar sesión 
+ */
 async function validateNavLogin() {
+    //Se revisa que los valores del input coincidan con el JSON
+    //Si está bien, se loggea
     if (correoLoginNav.value == loginInfo.mail && contraseniaLoginNav.value == loginInfo.password) {
         console.log("Logeado");
     }
+    //En caso contrario, le dice que no existe la cuenta
     else {
         event.preventDefault();
-        try{
         checkLoginDos.style.color= "#8B0003";
         checkLoginDos.innerHTML = "No tenemos esa cuenta de gomitas";
         console.log("Sólo abuehackerman puede entrar zi ke zi");
-        }
-        catch {
-            console.log("No kedó");
-        }
     }
 }
 
+/**
+ * Una función asíncrona que se realiza para compu, al dar click en iniciar sesión 
+ */
 async function validatePcLogin() {
+    //Se revisa que los valores del input coincidan con el JSON
+    //Si está bien, se loggea
     if (correoLoginPc.value == loginInfo.mail && contraseniaLoginPc.value == loginInfo.password) {
         console.log("Logeado");
     }
+    //En caso contrario, le dice que no existe la cuenta
     else {
         event.preventDefault();
-        try{
         checkLoginUno.style.color= "#8B0003";
         checkLoginUno.innerHTML = "No tenemos esa cuenta de gomitas";
         console.log("Sólo abuehackerman puede entrar zi ke zi");
-        }
-        catch {
-            console.log("No kedó");
-        }
     }
 }
+
+
+/**
+ * Esta función obtiene los datos de la URL.
+ */
+//(HOISTING POWAH!)
+ function fetchInfo(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> {
+        loginInfo=data;
+    }
+    )
+ }
+
+ //D. R. Y.
--- js/registro.js
+++ js/registro.js
@@ -1,46 +1,57 @@
 /**
- * Script para página de registro y login.
+ * Script para página de registro.
  * Creado por jatg-eca, abue, para GomGom
  * durante el Sprint#1: Front-end y maquetado
+ * 
+ * Este Script valida el registro de una persona usuaria
+ * 
+ * PODEROSÍSIMO TIIM JAVAJAVADOOOOOOOOO
  */
-console.log("Link");
 
+/*Se localiza el botón de registro
+y se le añade un event listener, que al dar click
+comienza la función asíncrona validarRegistro*/
 let botonRegistro = document.getElementById("boton-registro");
 botonRegistro.addEventListener("click", validarRegistro);
 
 
-
+/**
+ * La función asíncrona validarRegistro se enfoca en ver que las contraseñas
+ * coincidan
+ */
 async function validarRegistro() {
+    /*Se guarda el formulario de registro
+    y los valores en los inputs de contraseñas.
+    */
     let formularioRegistro = document.forms["registroFormulario"];
     let contraUno = formularioRegistro["validarContraUno"].value;
     let contraDos = formularioRegistro["validarContraDos"].value;
-    //let validacion = formularioRegistro["validacion"];
+    /*La variable de validación recuerda dónde se encuentra un párrafo para añadir información
+    coincidencia de las contraseñas*/
     let validacion = document.getElementById("validaContraTxt");
 
-    if (contraUno.length > 6) {
+    //Si la contraseña es igual o mayor a seis caracteres
+    if (contraUno.length >= 6) {
+        //...se revisa que ambas contraseñas del formulario estén bien
         if (contraUno == contraDos) {
             validacion.style.color= "green";
                 validacion.innerHTML = "¡Casi todo listo para las gomitas!";
           console.log("tan iguales");
-        } else {
+        } else {  //de lo contrario, evita el envío del formulario
             event.preventDefault();
-            try {
+           
                 validacion.style.color= "#8B0003";
                 validacion.innerHTML = "¡Ups! Creo que las contraseñas no son iguales...";
-            } catch (error) {
-                console.log("nanai");
-            }
+          
         }
     }
+    //Si la contraseña no tiene seis o más caracteres, se pide una nueva contra
     else {
         event.preventDefault();
-        try {
+     
             validacion.style.color= "#8B0003";
             validacion.innerHTML = "Para acceder por Gomitas, la contraseña debe tener 6 o más caracteres"
-        } catch (error) {
-            console.log("nanai");
-        }
+        
     }
 
 }
-
