eadcdcbfd6b6088e5eb9b3773c2f10cab1212fa2
JA Gonzalez <jatgonzalez33@gmail.com>
2022-08-11 20:56:32 -0500

    Add: Restore db

--- db/pedidos.sql
+++ db/pedidos.sql
@@ -1,7 +1,14 @@
-INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 2, "2022-10-01", 470.50, 1);
-INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 3, "2022-10-01", 470.50, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 2, "2022-10-01", 250.00, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 3, "2022-10-01", 250.00, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 1, "2022-10-01", 250.00, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 9, "2022-10-01", 250.00, 1);
+
 INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (4, 6, "2022-11-01", 750.00, 2);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (4, 6, "2022-11-01", 750.00, 2);
+
 INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (2, 1, "2022-08-26", 980.50, 1);
 INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (5, 4, "2022-09-15", 200.00, 2);
 
 SELECT * FROM gomgom.pedidos;
+SELECT * FROM gomgom.paquetes;
+SELECT * FROM gomgom.has_chamoy;
