15495e19c140c1caeeab1a7565a0f955e648fe37
JA Gonzalez <jatgonzalez33@gmail.com>
2022-08-11 22:50:59 -0500

    Add(SQLqueries): Nuevos joins

--- db/queries_test.sql
+++ db/queries_test.sql
@@ -12,8 +12,12 @@ NATURAL JOIN gomgom.gomitas ORDER BY numero_pedido;
 
 SELECT * FROM gomgom.pedidos;
 SELECT id_has_chamoy, fecha, venta_total FROM gomgom.pedidos INNER JOIN gomgom.paquetes ON paquetes_id_paquetes;
-SELECT * FROM gomgom.pedidos INNER JOIN gomgom.paquetes ON paquetes_id_paquetes INNER JOIN gomgom.nombre_paquete ON id_paquete;
 
-SELECT DISTINCT nombre_paquete FROM gomgom.paquetes INNER JOIN gomgom.nombre_paquete ON id_paquetes;
+-- ¿Qué paquetes hay y cuále es su precio?
+SELECT DISTINCT nombre_paquete AS Paketaxos, precio AS precio FROM gomgom.paquetes INNER JOIN gomgom.nombre_paquete ON id_paquetes;
 
-SELECT * FROM gomgom.gomitas;
+-- ¿Qué pedidos hay y qué gomitas llevan los paquetes que pidieron?
+SELECT numero_pedido, nombre_paquete, venta_total, fecha, gomitas.nombre FROM gomgom.nombre_paquete NATURAL JOIN gomgom.paquetes NATURAL JOIN gomgom.pedidos NATURAL JOIN gomgom.gomitas
+ORDER BY numero_pedido, nombre_paquete;
+
+SELECT * FROM gomgom.paquetes;
