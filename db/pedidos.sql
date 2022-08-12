8209dda3308c831037535a50880f1b130142abc1
JA Gonzalez <jatgonzalez33@gmail.com>
2022-08-11 20:51:59 -0500

    Check: Se guarda un punto de posible restauración para base de datos

---
+++ db/pedidos.sql
@@ -0,0 +1,7 @@
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 2, "2022-10-01", 470.50, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (6, 3, "2022-10-01", 470.50, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (4, 6, "2022-11-01", 750.00, 2);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (2, 1, "2022-08-26", 980.50, 1);
+INSERT INTO gomgom.pedidos(`paquetes_id_paquetes`, `gomitas_id_gomitas`, `fecha`, `venta_total`, `id_has_chamoy`) VALUES (5, 4, "2022-09-15", 200.00, 2);
+
+SELECT * FROM gomgom.pedidos;
---
+++ db/ventas.sql
@@ -0,0 +1,6 @@
+INSERT INTO gomgom.ventas(`id_usuario`, `id_pedidos`) VALUES (3, 2);
+INSERT INTO gomgom.ventas(`id_usuario`, `id_pedidos`) VALUES (2, 3);
+INSERT INTO gomgom.ventas(`id_usuario`, `id_pedidos`) VALUES (5, 4);
+INSERT INTO gomgom.ventas(`id_usuario`, `id_pedidos`) VALUES (7, 1);
+
+SELECT * FROM gomgom.ventas;
