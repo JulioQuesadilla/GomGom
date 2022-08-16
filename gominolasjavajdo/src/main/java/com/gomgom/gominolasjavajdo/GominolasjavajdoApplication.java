cb62c9e77fcb7b7b659260849b4aeefded02b56e
JA Gonzalez <jatgonzalez33@gmail.com>
2022-08-16 12:34:58 -0500

    New/Move: Se añade carpeta de Java para API y backend. Movidos páginas estáticas

---
+++ gominolasjavajdo/src/main/java/com/gomgom/gominolasjavajdo/GominolasjavajdoApplication.java
@@ -0,0 +1,13 @@
+package com.gomgom.gominolasjavajdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GominolasjavajdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GominolasjavajdoApplication.class, args);
+	}
+
+}
