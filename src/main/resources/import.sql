b2164ec29cc118c31e82c888d5d26d5108e353e6
JA Gonzalez <jatgonzalez33@gmail.com>
2022-08-18 21:20:33 -0500

    New(DB): Cambios de DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -74,67 +74,30 @@ INSERT INTO has_chamoy(has_chamoy) VALUES ('Sí tiene chamoy');
 
 
 
---ALTER TABLE pedidos AUTO_INCREMENT= 20;
--- Paquetes de un cliente
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 2, '2022-10-01', 500.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 3, '2022-10-01', 500.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 1, '2022-10-01', 500.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 9, '2022-10-01', 500.00, 1);
-
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 9, '2022-10-01', 500.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 6, '2022-10-01', 500.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (1, 6, 7, '2022-10-01', 500.00, 1);
-
- -- Paquetes de otrx clientx
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 6, '2022-11-01', 760.00, 2);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 3, '2022-11-01', 760.00, 2);
-
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 6, '2022-11-01', 760.00, 2);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 3, '2022-11-01', 760.00, 2);
-
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 6, '2022-11-01', 760.00, 2);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 5, 3, '2022-11-01', 760.00, 2);
-
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 4, 6, '2022-11-01', 760.00, 2);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (2, 4, 3, '2022-11-01', 760.00, 2);
+INSERT INTO pedidos (fecha, venta_total, id_has_chamoy) VALUES ('2022-08-17', '510', '2');
+INSERT INTO pedidos (fecha, venta_total, id_has_chamoy) VALUES ('2022-08-16', '250', '1');
+INSERT INTO pedidos (fecha, venta_total, id_has_chamoy) VALUES ('2022-08-16', '200', '1');
 
 
--- Paquetes de cliente
+INSERT INTO pedidos_pack (order_id_pedidos, pack_id_paquetes) VALUES ('1', '6');
+INSERT INTO pedidos_pack (order_id_pedidos, pack_id_paquetes) VALUES ('1', '6');
+INSERT INTO pedidos_pack (order_id_pedidos, pack_id_paquetes) VALUES ('2', '1');
+INSERT INTO pedidos_pack (order_id_pedidos, pack_id_paquetes) VALUES ('2', '5');
+INSERT INTO pedidos_pack (order_id_pedidos, pack_id_paquetes) VALUES ('3', '5');
 
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (3, 2, 1, '2022-08-26', 75.00, 1);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (3, 2, 1, '2022-08-26', 75.00, 1);
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('1', '5');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('1', '7');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('1', '8');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('1', '4');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('1', '2');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('2', '5');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('2', '9');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('2', '1');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('3', '4');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('3', '6');
+INSERT INTO pedidos_gummies (order_id_pedidos, gummies_id_gomitas) VALUES ('3', '10');
 
 
--- Paquetes de cliente
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (4, 5, 4, '2022-09-15', 210.00, 2);
-INSERT INTO pedidos(numero_pedido, paquetes_id_paquetes, gomitas_id_gomitas, fecha, venta_total, id_has_chamoy) VALUES (4, 5, 10, '2022-09-15', 210.00, 2);
-
-
-
-
--- Ventas de un pedido
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '1');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '2');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '3');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '4');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '5');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '6');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('5', '7');
-
- -- Ventas de otro pedido
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '8');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '9');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '10');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '11');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '12');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '13');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '14');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('2', '15');
-
- -- Ventas del tercer pedido
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('3', '16');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('3', '17');
-
--- último pedido vendido
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('4', '18');
-INSERT INTO ventas(id_usuario, id_pedidos) VALUES ('4', '19');
+INSERT INTO ventas (id_pedidos, id_usuario) VALUES ('1', '4');
+INSERT INTO ventas (id_pedidos, id_usuario) VALUES ('2', '2');
+INSERT INTO ventas (id_pedidos, id_usuario) VALUES ('3', '3');
